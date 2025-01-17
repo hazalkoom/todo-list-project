e27f6154c23f9a118231967d0bcf9397c9f1f566
hazalkoom <mwhamd345@gmail.com>
2025-01-17 08:48:46 +0200

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,70 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from todo import models
+from todo.models import ToDo
+from django.contrib.auth.decorators import login_required
+
+
+
+
+def singup(request):
+    if request.method=='POST':
+        fnm = request.POST.get('fnm')
+        emailid= request.POST.get('emailid')
+        pwd = request.POST.get('pwd')
+        print(fnm, emailid, pwd)
+        my_user = User.objects.create_user(fnm, emailid, pwd)
+        my_user.save()
+        return redirect('/loginn')
+        
+    return render(request, 'singup.html')
+
+def loginn(request):
+    if request.method=='POST':
+        fnm = request.POST.get('fnm')
+        pwd = request.POST.get('pwd')
+        print(fnm, pwd)
+        userr = authenticate(request, username=fnm, password=pwd)
+        if userr is not None:
+            login(request, userr)
+            return redirect('/todopage')
+        else:
+            return redirect('/loginn')
+    return render(request, 'login.html')
+
+@login_required(login_url='/loginn')
+def todo(request):
+    if request.method=='POST':
+        title = request.POST.get('title')
+        print(title)
+        obj = ToDo(title=title, user=request.user)
+        obj.save()
+        user = request.user
+        res = ToDo.objects.filter(user=request.user).order_by('-date')
+        return redirect('/todopage', {'res':res})
+    res = ToDo.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'todo.html', {'res':res})
+
+@login_required(login_url='/loginn')
+def edit_todo(request, srno):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        print(title)
+        obj = ToDo.objects.get(srno=srno)
+        obj.title = title
+        obj.save()
+        return redirect('/todopage')
+    obj = ToDo.objects.get(srno=srno)
+    return render(request, 'edit_todo.html', {'obj': obj})
+
+@login_required(login_url='/loginn')
+def delete_todo(request, srno):
+    obj = ToDo.objects.get(srno=srno)
+    obj.delete()
+    return redirect('/todopage')
+
+def singout(request):
+    logout(request)
+    return redirect('/loginn')
